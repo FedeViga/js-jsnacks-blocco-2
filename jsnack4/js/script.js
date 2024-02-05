6f58951edb48ff58b614bfa81084cfa5c6c146aa
federico_viganò <fede_viga@hotmail.it>
2024-02-05 19:33:17 +0100

    completato snack 4 - Fine esercizio

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,2 +1,17 @@
 // Snack 4
-// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+// Creo un ciclo infinito in cui chiedo all'utente di inserire un numero, per uscire dal ciclo l'utente
+// dovrà scrivere la parola "Esci"
+
+let isNumber = false;
+
+do {
+    const userNumber = prompt("inserisci un numero oppure scrivi 'Esci' per uscire");
+    console.log(userNumber);
+    if (userNumber == "Esci") {
+        isNumber = false;
+    } else if (isNaN(userNumber)) {
+        alert("Errore: inserisci un numero oppure scrivi 'Esci' per uscire");
+    } else {
+        isNumber = true;
+    }
+} while (isNumber == true);
